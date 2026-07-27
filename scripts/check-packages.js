bac8e9370074a459497b5af4c5d13dae695fa8d7
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-07-27 21:08:57 +0200

    chore: update dev deps

--- scripts/check-packages.js
+++
@@ -1,7 +0,0 @@
-import { createCheckPackageWithWorkspaces } from "check-package-dependencies";
-
-await createCheckPackageWithWorkspaces({
-  isLibrary: (pkg) => !pkg.name.startsWith("@todo-example"),
-})
-  .checkRecommended()
-  .run();
